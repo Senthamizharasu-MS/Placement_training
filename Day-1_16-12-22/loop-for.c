b7dcaddaaaa6ad5a9981be1c334e98620d84bde6
Senthamizharasu-MS <senthamizharasum.ms2020cse@sece.ac.in>
2022-12-19 11:31:56 +0530

    Day-1 training pushed

---
+++ Day-1_16-12-22/loop-for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    for(a=1;a++ <=1;a++)
+    {
+        for(a++;a++ <=6;a++)
+        {
+            a++;
+        }
+    }
+    printf("%d",a);
+}
---
+++ Day-1_16-12-22/power_of_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number : ")
+    scanf("%d",&n);
+    
+    if(n>0)
+    {
+        if(n&n-1)
+        {
+            printf("no");
+        }
+        else
+        {
+            printf("yes");
+        } 
+    }
+    else
+    {
+        printf("no");
+    }
+    
+    return 0;
+}
